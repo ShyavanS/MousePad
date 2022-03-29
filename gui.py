01c5c9de5e49597b672bd123fd7c948f879e2629
Berserkered <sohampatel2003@gmail.com>
2022-03-29 02:45:05 -0400

    Finished gui, Visual Changes and Link to main code

--- gui.py
+++ gui.py
@@ -24,6 +24,54 @@ class MyLayout(TabbedPanel):
     def cursor_sen(self, *args):
         self.cursor_text.text = str(int(args[1]))
         self.cursor_text.font_size = 8
+
+    def vibration_time(self, value):
+        self.ids.vibration_id.text = value
+
+    def controller_num(self, value):
+        self.ids.controller_id.text = value
+
+    def left_joystick(self, value):
+        self.ids.left_stick.text = value
+
+    def press_up(self, value):
+        self.ids.up_button.text = value
+
+    def press_left(self, value):
+        self.ids.left_button.text = value
+
+    def press_right(self, value):
+        self.ids.right_button.text = value
+
+    def press_down(self, value):
+        self.ids.down_button.text = value
+
+    def press_l1(self, value):
+        self.ids.l1_button.text = value
+
+    def press_l2(self, value):
+        self.ids.l2_button.text = value
+
+    def press_y(self, value):
+        self.ids.y_button.text = value
+
+    def press_x(self, value):
+        self.ids.x_button.text = value
+
+    def press_b(self, value):
+        self.ids.b_button.text = value
+
+    def press_a(self, value):
+        self.ids.a_button.text = value
+
+    def right_joystick(self, value):
+        self.ids.right_stick.text = value
+
+    def press_r2(self, value):
+        self.ids.r1_button.text = value
+
+    def press_r2(self, value):
+        self.ids.r2_button.text = value
     pass
 
 
