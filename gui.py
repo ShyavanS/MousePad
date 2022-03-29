724bb9c5aa2d6929192cd72fc3038166f9986465
Berserkered <sohampatel2003@gmail.com>
2022-03-29 01:50:43 -0400

    Updated gui with kivy

--- gui.py
+++ gui.py
@@ -1,12 +1,7 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.tabbedpanel import TabbedPanel
 
 
@@ -14,13 +9,28 @@ Builder.load_file('tabs.kv')
 
 
 class MyLayout(TabbedPanel):
+    def left_vib(self, *args):
+        self.left_text.text = str(int(args[1]))
+        self.left_text.font_size = 8
+
+    def right_vib(self, *args):
+        self.right_text.text = str(int(args[1]))
+        self.right_text.font_size = 8
+
+    def scroll_sen(self, *args):
+        self.scroll_text.text = str(int(args[1]))
+        self.scroll_text.font_size = 8
+
+    def cursor_sen(self, *args):
+        self.cursor_text.text = str(int(args[1]))
+        self.cursor_text.font_size = 8
     pass
 
 
-class MyGui(App):
+class MousePad(App):
     def build(self):
         return MyLayout()
 
 
 if __name__ == '__main__':
-    MyGui().run()
+    MousePad().run()
